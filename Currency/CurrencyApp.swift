b71e686a353025a7e1bf64b25477ca8ffc2788a5
benson <benson@myapps.finance>
2021-04-12 14:04:33 +0700

    Initial Commit

---
+++ Currency/CurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyApp.swift
+//  Currency
+//
+//  Created by Benson on 4/12/21.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
